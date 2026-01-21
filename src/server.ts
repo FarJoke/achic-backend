e6ea47377bcbe9df309fe6cfc436c3ee6adf12f4
Дубовой Олег <dubovoyo@inlinegroup-c.ru>
2026-01-21 20:12:17 +0300

    Feat: DB env

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import app from "./app";
+import { connectDB } from "./config/db";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+  });
+};
+
+startServer();
